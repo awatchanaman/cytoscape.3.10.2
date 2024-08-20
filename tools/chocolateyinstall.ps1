48b2987dbac710dab1fd700d680322de1f310948
awatchanaman <94679765+awatchanaman@users.noreply.github.com>
2024-08-20 14:25:42 +0200

    Initial commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName    = 'cytoscape'
+  softwareName   = 'Cytoscape*'
+
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  fileType       = 'exe'
+
+  url64bit       = 'https://github.com/cytoscape/cytoscape/releases/download/3.10.2/Cytoscape_3_10_2_windows_64bit.exe'
+
+  silentArgs     = '-q'
+  validExitCodes = @(0)
+
+  checksum64     = '8E2B92E8F01CCF2FEE97DCBDD9FB31CACE4F45E6298B8504A0EBE937D46107BB'
+  checksumType64 = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
